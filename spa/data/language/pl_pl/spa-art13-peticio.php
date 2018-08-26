3fafef53bba66b19acc9387bbc6c351fc6006689
Jan F Kowalski <jan@secalith.co.uk>
2018-08-26 04:13:46 +0100

    [PR] added stopacta logo

--- spa/data/language/pl_pl/spa-art13-peticio.php
+++ spa/data/language/pl_pl/spa-art13-peticio.php
@@ -1,12 +1,18 @@
 <?php
 return [
     'Name and Surname' => 'Imię i Nazwisko',
-    'Signature has been recorded.' => 'Twoje wsparcie zostało zarejestrowane',
-    'Thank you for your support!' => 'Dziękujemy za wsparcie!',
+    'Signature has been recorded.' => 'Twoje wsparcie zostało zarejestrowane.',
+    'Thank you for your support!' => 'Dziękujemy za glos!',
     'Sign the Petition' => 'Podpisz Petycję',
     'Events' => 'Wydarzenia',
     'Information' => 'Informacje',
     '(required)' => '(wymagane)',
-    'I’d like to hear about news from #stopACTA2' => 'Chciałbym/Chciałabym otrzymywać Newsletter od #stopACTA2',
-    'I agree to the terms of service and privacy policy.' => 'Zgadzam się na warunki serwisu oraz zapoznałem/zapoznałam się z polityką prywatności',
+    'I’d like to hear about news from #stopACTA2' => 'Chciałbym/Chciałabym otrzymywać Newsletter od #stopACTA2.',
+    'I agree to the terms of service and privacy policy.' => 'Zgadzam się na warunki serwisu oraz zapoznałem/zapoznałam się z polityką prywatności.',
+    'How else could you help?' => 'Jak jeszcze mozna pomoc?',
+    'Share this Petition with Friends.' => 'Poinformuj znajomych na Facebook`u.',
+    'Like our facebook pages, so we can reach more people.' => 'Polub nasza strone #stopACTA2, dzieki czemu dotrzemy do wiekszej ilosci ludzi.',
+    'Increase awarness by having conversations with friends and colleagues.' => 'Zwieksz swiadomosc poprzez rozmowy ze znajomymi i kolegami.',
+    'Join us on our protests and rallies.' => 'Wez udzial w naszych protestach i marszach #stopACTA2.',
+
 ];
