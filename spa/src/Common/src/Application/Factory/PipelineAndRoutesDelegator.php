c7dd8d68ba6ca1624ebc528054b6845917a383c7
Jan F Kowalski <jan@secalith.co.uk>
2018-08-28 03:16:08 +0100

    [FIX] stopacta link

--- spa/src/Common/src/Application/Factory/PipelineAndRoutesDelegator.php
+++ spa/src/Common/src/Application/Factory/PipelineAndRoutesDelegator.php
@@ -52,7 +52,7 @@ class PipelineAndRoutesDelegator
 
         if($hostname==='www.art13.eu' || $hostname==='art13.eu') {
             if($container->get(\I18n\Service\I18n::class)->getCurrentLanguage()==="pl_pl") {
-                header("Location: http://www.stopacta2.org/o_stopacta2/");
+                header("Location: http://www.stopacta2.org");
                 die();
             }
         }
